9591cc065e0b3fe353de2a59e21cc5b6832b7b4a
DESKTOP-DOKPC8B\Olaf <o.purzycki@gmail.com>
2019-02-16 11:57:36 +0100

    create vookstore jdbc example

---
+++ src/main/java/sda/Book.java
@@ -0,0 +1,28 @@
+package sda;
+
+import java.util.Objects;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/test/java/sda/BookstoreTest.java
@@ -0,0 +1,23 @@
+package sda;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+public class BookstoreTest {
+@DisplayName("should add new book")
+@Test
+    void test()throws Exception{
+    //given
+    BookStoreManager manager = new BookStoreManager();
+
+    //when
+    manager.add("Thinking in Java", "Bruce Eckel");
+
+    //then
+    Collection<Book> allBooks = manager.findAll();
+    Assertions.assertThat(allBooks).hasSize(1);
+    }
+}
